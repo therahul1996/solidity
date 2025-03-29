cb5fa827080cacf4e891d6eb90f48fd6a0a67a11
therahul1996 <rs.official1996@gmail.com>
2025-03-29 22:20:09 +0530

    feat: twitter

---
+++ contracts/Twitter.sol
@@ -0,0 +1,13 @@
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.8.0;
+
+contract Twitter {
+    mapping(address => string) public tweets;
+
+    function createTweet(string memory _tweet) public {
+        tweets[msg.sender] = _tweet;
+    }
+    function getTweets(address _owner) public view returns (string memory) {
+        return tweets[_owner];
+    }
+}
